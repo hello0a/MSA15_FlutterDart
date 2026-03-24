e228c4922726779c67aee6e53613ff951c2f4daa
hello0a <aya70713@gmail.com>
2026-03-24 11:57:06 +0900

    레이아웃

--- layout_widget/lib/main.dart
+++ layout_widget/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:layout_widget/screens/detail_screen.dart';
+import 'package:layout_widget/screens/gridview_screen.dart';
 import 'package:layout_widget/screens/listview_screen.dart';
 
 void main() {
@@ -12,7 +14,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ListviewScreen()
+      title: '레이아웃 위젯',
+      home: 
+      ListviewScreen()
+      // GridviewScreen()
+      // DetailScreen()
     );
   }
 }
--- layout_widget/lib/screens/listview_screen.dart
+++ layout_widget/lib/screens/listview_screen.dart
@@ -1,6 +1,7 @@
 // stfl
 import 'package:flutter/material.dart';
 import 'package:layout_widget/models/product.dart';
+import 'package:layout_widget/screens/detail_screen.dart';
 
 class ListviewScreen extends StatefulWidget {
   const ListviewScreen({super.key});
@@ -12,6 +13,7 @@ class ListviewScreen extends StatefulWidget {
 class _ListviewScreenState extends State<ListviewScreen> {
 
   // 1. 상품 객체 위젯 리스트
+  // 요소 Widget / ListTile
   final List<Widget> productWidgetList = 
     List.generate(10, (index) => ListTile(
       leading: const Icon(Icons.label),
@@ -24,6 +26,7 @@ class _ListviewScreenState extends State<ListviewScreen> {
     ));
 
   // 2. 상품 객체 리스트
+  // 요소 Product / Product
   // 1) yaml assets: -image/ 추가
   // 2) get Package 클릭
   // 3) 다시 시작 클릭
@@ -64,7 +67,43 @@ class _ListviewScreenState extends State<ListviewScreen> {
                   ),
                   trailing: const Icon(Icons.arrow_forward),
                   onTap: () {
-                    print('아이템 클릭');
+                    // print('아이템 클릭');
+                    AlertDialog dialog = AlertDialog(
+                      content: Text(
+                        "상품명 : ${product.title}",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            // 1. pushReplacement와 동일
+                            // // AlertDialog를 스크린에서 제거
+                            // Navigator.of(context).pop();
+                            // // 상세 화면으로 이동
+                            // Navigator.push(
+                            //   context, 
+                            //   MaterialPageRoute(
+                            //     builder: (context) => DetailScreen(product: product)
+                            //   )
+                            // );
+                            // 2. 현재 최상위 스크린(Alert)을 새 스크린(상세)으로 대체
+                            // -> 쌓이는게 아니라 아예 스크린이 전환되어야한다! 의 경우 사용
+                            Navigator.pushReplacement(
+                              context, 
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(product: product)
+                              )
+                            );
+
+                          }, 
+                          child: const Text("확인")
+                        )
+                      ],
+                    );
+                    showDialog(
+                      context: context, 
+                      builder: (BuildContext context) => dialog
+                    );
                   },
                 );
               }
